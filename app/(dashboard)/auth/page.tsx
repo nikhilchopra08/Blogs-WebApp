67894580b80757550e1aaae1f68403ba3237f235
Nikhil chopra <nikhil0080012005@gmail.com>
2024-08-13 13:58:03 +0530

    Added : Ui of Register and Login

--- app/(dashboard)/auth/page.tsx
+++ app/(dashboard)/auth/page.tsx
@@ -1,13 +1,19 @@
 "use client"
 
+import Input from "@/Components/input";
 import { useCallback, useState } from "react";
 
 const Auth = () => {
+    const [name , setName] = useState('');
+    const [email , setEmail] = useState('');
+    const [password , setPassword] = useState('');
+
     const [variant , setVariant] = useState('login');
 
     const toggleVariant = useCallback(() => {
-        setVariant((currentVariant) => currentVariant === 'login' ? 'logout' : 'login')
+        setVariant((currentVariant) => currentVariant === 'login' ? 'Register' : 'login')
     } , [])
+
 
     return(
         <div className="relative bg-slate-50 h-[100vh] w-[100vw]">
@@ -18,9 +24,31 @@ const Auth = () => {
 
                 <div className="flex justify-center">
                     <div className="bg-white bg-opacity-100 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-                        <h1 className="text-black">
-                            {variant == 'login' ? 'login' : 'logout'}
+                        <h1 className="text-black text-center">
+                            {variant == 'login' ? 'Login' : 'Register'}
                         </h1>
+                        <div className="flex flex-col gap-4">
+                            {variant === 'login' && (
+                                <Input 
+                                    label="name"
+                                    onChange={(e : any) => setName(e.target.value)}
+                                    id="name"
+                                    value={name}
+                                />
+                            )}
+                                <Input 
+                                    label="Email"
+                                    onChange={(e : any) => setEmail(e.target.value)}
+                                    id="Email"
+                                    value={email}
+                                />
+                                <Input 
+                                    label="Password"
+                                    onChange={(e : any) => setPassword(e.target.value)}
+                                    id="Password"
+                                    value={password}
+                                />
+                        </div>
                         <button className="bg-green-500" onClick={toggleVariant}>Toggle</button>
                     </div>
                 </div>
